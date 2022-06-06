7d6af39c3e36ac68cd8d245cbff58e8f1c9c1774
Alysson Bondade Lopes <bondadelopes2006@gmail.com>
2022-06-06 14:37:59 -0300

    prova atualizada

--- api/src/controller/animecontroller.js
+++ api/src/controller/animecontroller.js
@@ -5,8 +5,27 @@ import { Router } from 'express'
 
 const server = Router();
 
-server.post('/animes', async (req, resp) => {
+
+
+server.get('/animes', (req, resp) => {
     try{
         
+    }
+})
+
+
+
+
+server.post('/animes/postar', async (req, resp) => {
+    try{
+        const { nome } = req.body;
+        const resposta = await Anime(nome);
+
+        resp.send(resposta);
+    }
+    catch(err) {
+        resp.status(401).send({
+            erro: err.message
+        })
     }
 })
